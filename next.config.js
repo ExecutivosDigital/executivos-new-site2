1726762dc0395bf883b894161f90f2ad41a850dd
Gabriel Antonio <mouragabriel205@gmail.com>
2023-08-17 12:35:52 -0400

    commit

--- next.config.js
+++ next.config.js
@@ -2,6 +2,13 @@ const path = require("path");
 
 module.exports = {
   reactStrictMode: false,
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
   },
