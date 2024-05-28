6008fcd0f851088ad150fc9dca1eadcdce863b4f
maycon600 <mayconvasconcellos2010@hotmail.com>
2024-05-27 21:38:06 -0400

    ADD NEW TAG

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -4,6 +4,7 @@ import GlobalStyle from "../styles/global";
 import { ThemeProvider } from "styled-components";
 import Theme from "../styles/global";
 import React from "react";
+import Script from "next/script";
 import "../index.css";
 import Head from "next/head";
 function MyApp({ Component, pageProps }: any) {
@@ -14,25 +15,39 @@ function MyApp({ Component, pageProps }: any) {
 
   return (
     <>
-        <Head>
+      <Head>
         <meta charSet="utf-8" />
         <title>Executivo's Digital</title>
         <link rel="canonical" href="" />
-        <script src="https://www.googletagmanager.com/gtag/js?id=G-E5BHR0PGS3" />
-        <script id="google-analytics">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-  
-            gtag('config', 'G-E5BHR0PGS3');
-          `}
-        </script>
-        </Head>
-      
-        <GlobalStyle />
-        <Component {...pageProps} />
-        
+      </Head>
+      {/* Google Tag Manager */}
+      <Script
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-5NWHV3RK');
+        `,
+        }}
+      />
+      {/* Microsoft Clarity */}
+      <Script
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+          (function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "miu5womvj1");
+        `,
+        }}
+      />
+      <GlobalStyle />
+      <Component {...pageProps} />
     </>
   );
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,10 +3,18 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head >
-       
-      </Head>
+      <Head></Head>
       <body>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5NWHV3RK"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         <Main />
         <NextScript />
       </body>
